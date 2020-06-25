68ae7d64fbd71fc712b037ea114e8c791a28a536
Brandon Baskett <bbaskett@Brandons-MBP.verizon.net>
2020-06-25 17:01:40 -0400

    v1.0.8: CSS updates on Counter module

--- src/components/Counter.svelte
+++ src/components/Counter.svelte
@@ -7,14 +7,15 @@
     width: 100%;
     padding: 0.75rem 1rem;
     border-radius: 0.25rem;
-    background-color: hsl(0, 0%, 95%);
     box-shadow: 0 0.25rem 0.25rem 0 hsla(0, 0%, 50%, 0.35);
     border: 1px solid hsl(0, 0%, 65%);
+    background-color: hsl(0, 0%, 95%);
   }
 
   td {
     text-align: center;
     font-size: 0.75rem;
+    font-weight: 500;
   }
 </style>
 
